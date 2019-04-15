4e74d234bdf75a75eb7d7b869758d636f7d130ab
David Schmidt <david.adam.schmidt@gmail.com>
2019-04-15 18:21:16 +0200

    implement user story 102 "display a question"

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,4 @@
-import csv
+import connection
 
 
 ANSWERS_HEADER = [
@@ -30,3 +30,20 @@ def read_csv():
 # write data to csv
 def write_to_csv():
     pass
+
+
+def get_question_by_id(question_id):
+    questions = connection.read_csv("data/question.csv")
+    for question in questions:
+        if question["id"] == question_id:
+            return question
+
+
+def get_answers_by_question_id(qustion_id):
+    answers = connection.read_csv("data/answer.csv")
+    return [x for x in answers if x["question_id"] == qustion_id]
+
+
+def convert_headers_to_user_friendly(headers):
+    for i in range(len(headers)):
+        headers[i] = headers[i].capitalize().replace("_", " ")
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template
+import data_manager
 
 
 app = Flask(__name__)
@@ -9,9 +10,26 @@ def route_questions_list():
     pass
 
 
-@app.route("/question/<question_id")
-def route_question_disp(question_id):
-    pass
+@app.route("/question/<question_id>")
+def route_question_display(question_id):
+    template_name = "question.html"
+    question = data_manager.get_question_by_id(question_id)
+    if question is None:
+        return render_template(template_name, question_id=question_id)
+    question_headers = list(question.keys())
+    data_manager.convert_headers_to_user_friendly(question_headers)
+    answers = data_manager.get_answers_by_question_id(question_id)
+    answer_headers = None
+    if len(answers) > 0:
+        answer_headers = list(answers[0].keys())
+        data_manager.convert_headers_to_user_friendly(answer_headers)
+    return render_template(
+        template_name,
+        question=question,
+        question_headers=question_headers,
+        answers=answers,
+        answer_headers=answer_headers
+    )
 
 
 @app.route("/add-question")
