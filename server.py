d35c929927c1bb7933879bc486a0de9d4961c241
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-04-16 02:13:28 +0200

    improve add_question function

--- server.py
+++ server.py
@@ -6,8 +6,23 @@ import time
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/list", methods=['GET', 'POST'])
+@app.route("/list")
 def route_questions_list():
+    return render_template('index.html')
+
+
+@app.route("/question/<question_id>", methods=['GET', 'POST'])
+def route_question_display(question_id):
+    template_name = "question.html"
+    question = data_manager.get_question_by_id(question_id)
+    if question is None:
+        return render_template(template_name, question_id=question_id)
+    answers = data_manager.get_answers_by_question_id(question_id)
+    return render_template(template_name, question=question, answers=answers)
+
+
+@app.route("/add-question", methods=['GET', 'POST'])
+def route_question_add():
     if request.method == 'POST':
         id_num = len(data_manager.read_csv('data/question.csv'))
         submission_time = int(time.time())
@@ -21,21 +36,7 @@ def route_questions_list():
         headers = [id_num, submission_time, view_number, vote_number,
                    title, message, image]
         data_manager.write_to_csv('data/question.csv', headers, fieldnames)
-    return render_template('add_question.html')
-
-
-@app.route("/question/<question_id>")
-def route_question_display(question_id):
-    template_name = "question.html"
-    question = data_manager.get_question_by_id(question_id)
-    if question is None:
-        return render_template(template_name, question_id=question_id)
-    answers = data_manager.get_answers_by_question_id(question_id)
-    return render_template(template_name, question=question, answers=answers)
-
-
-@app.route("/add-question")
-def route_question_add():
+        return redirect('/question/%s' % id_num)
     return render_template('add_question.html')
 
 
