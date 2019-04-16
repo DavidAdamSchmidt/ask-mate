0a47b6d64af9a86837909f10f416ab46c7e3e408
Jankaral <janoskrizsan402@gmail.com>
2019-04-16 10:34:34 +0200

    minor changes

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import csv
 # id_type refers to the key of the id which you want to refer to
 def get_latest_id(filename, id_type):
     new_id = 0
-    with open(filename, 'r+', newline='') as datafile:
+    with open(filename, 'r', newline='') as datafile:
         datafile.seek(0)
         reader = csv.DictReader(datafile)
         for row in reader:
@@ -17,21 +17,6 @@ def read_csv(filename):
     with open(filename, "r", newline="") as datafile:
         reader = csv.DictReader(datafile)
         return [row for row in reader]
-
-
-def write_to_csv(filename, headers, fieldnames):
-    with open(filename, 'a') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-        # if len(fieldnames) == 6:
-        #         writer.writerow({fieldnames[0] : headers[0], fieldnames[1] : headers[1],
-        #         fieldnames[2] : headers[2], fieldnames[3] : headers[3],
-        #         fieldnames[4] : headers[4], fieldnames[5] : headers[5]})
-        # elif len(fieldnames) == 7:
-        writer.writerow({fieldnames[0] : headers[0], fieldnames[1] : headers[1],
-        fieldnames[2] : headers[2], fieldnames[3] : headers[3],
-        fieldnames[4] : headers[4], fieldnames[5] : headers[5], fieldnames[6] : headers[6]})
-        return 'Thanks for adding a question'
 
 
 def update_to_csv(filename, updated_qna, headers):
