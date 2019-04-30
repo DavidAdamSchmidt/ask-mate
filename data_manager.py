d893a3aeae01b4cf71d305c06a97349efba06bbd
Jankaral <janoskrizsan402@gmail.com>
2019-04-30 09:27:51 +0200

    minor changes

--- data_manager.py
+++ data_manager.py
@@ -1,85 +1,30 @@
 import connection
-
-
-
-ANSWERS_HEADER = [
-    "id",
-    "submission_time",
-    "vote_number",
-    "question_id",
-    "message",
-    "image"
-]
-
-
-QUESTIONS_HEADER = [
-    "id",
-    "submission_time",
-    "view_number",
-    "vote_number",
-    "title",
-    "message",
-    "image"
-]
+import database_common
 
 
 def update_vote_number(id_, increase_by, answer=False):
-    record = get_record_by_id(id_, answer=answer)
-    vote_number = int(record["vote_number"])
-    record["vote_number"] = vote_number + increase_by
-    if answer:
-        connection.update_to_csv("data/answer.csv", record, ANSWERS_HEADER)
-    else:
-        connection.update_to_csv("data/question.csv", record, QUESTIONS_HEADER)
+    pass
 
 
-def read_csv(filename):
-    return connection.read_csv(filename)
+def insert_new_record(filename, headers, fieldnames):
+    pass
 
 
-def write_new_to_csv(filename, headers, fieldnames):
-    connection.write_new_to_csv(filename, headers, fieldnames)
-
-
-def update_to_csv(filename, updated_qna, headers):
-    connection.update_to_csv(filename, updated_qna, headers)
-
-
-def get_question_by_id(question_id):
-    questions = connection.read_csv("data/question.csv")
-    for question in questions:
-        if question["id"] == str(question_id):
-            return question
+def update_record(filename, updated_qna, headers):
+    pass
 
 
 def get_record_by_id(id_, answer=False):
-    filename = f"data/{'answer' if answer else 'question'}.csv"
-    records = connection.read_csv(filename)
-    for record in records:
-        if record["id"] == id_:
-            return record
+    pass
 
 
 def get_answers_by_question_id(question_id):
-    answers = connection.read_csv("data/answer.csv")
-    return [x for x in answers if x["question_id"] == question_id]
+    pass
 
 
 def sort_by_any(filename, header_by, reverse_):
-    table = connection.read_csv(filename)
-    table.sort(key=lambda x: int(x[header_by]) if x[header_by].lstrip(
-        "-").isdigit() else x[header_by], reverse=reverse_)
-    return table
+    pass
 
 
 def delete_by_id(id_to_del, id_type, answer=False):
-    filename = f"data/{'answer' if answer else 'question'}.csv"
-    table = connection.read_csv(filename)
-    table = [x for x in table if x[id_type] != id_to_del]
-    for row in table:
-        if int(row[id_type]) > int(id_to_del):
-            row[id_type] = int(row[id_type]) - 1
-    connection.write_data_(
-        filename,
-        table,
-        ANSWERS_HEADER if answer else QUESTIONS_HEADER)
+    pass
