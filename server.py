3e44c48680a7d3b6cbb0bdcee85581d172b7eb36
Jankaral <janoskrizsan402@gmail.com>
2019-05-02 13:31:44 +0200

    tags done, delete button

--- server.py
+++ server.py
@@ -140,8 +140,14 @@ def route_search_results():
 
 
 @app.route("/question/<question_id>/add-edit-tag", methods=['GET', 'POST'])
-def add_edit_tag(question_id):
-    tags = data_manager.get_basic_tags()
+def route_add_edit_tag(question_id):
+    tags = data_manager.BASIC_TAGS
+    tag_id = data_manager.get_record_by_question_id(question_id, 'question_tag')
+    if tag_id == []:
+        tag_id = None
+    else:
+        tag_id = tag_id[0]['tag_id']
+        tag_id = data_manager.get_record_by_id(tag_id, 'tag')
     if request.method == "POST":
         ntag = request.form.to_dict()
         question_id = ntag['question_id']
@@ -151,15 +157,18 @@ def add_edit_tag(question_id):
         else:
             del ntag['select']
             ntag['name'] = ntag.pop('define_own')
-        data_manager.insert_new_tag(ntag)
-        return redirect("/question/" + question_id)
-    return render_template('add_edit_tag.html', question_id=question_id, tags=tags)
+        if ntag['tag_id'] == "":
+            data_manager.insert_new_tag(ntag)
+        else:
+            data_manager.update_tag(ntag)
+        return redirect(f"/question/{question_id}")
+    return render_template('add_edit_tag.html', question_id=question_id, tags=tags, tag_id=tag_id)
 
 
 @app.route("/question/<question_id>/<tag>/delete")
-def delete_tag(question_id, tag):
+def route_delete_tag(question_id, tag):
     data_manager.delete_tags(question_id, tag)
-    return redirect("/question/" + question_id)
+    return redirect(f"/question/{question_id}")
 
 
 if __name__ == "__main__":
