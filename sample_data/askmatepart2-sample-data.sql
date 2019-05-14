f884476481e94212cff736e31d0f0c64eb9598b7
David Schmidt <david.adam.schmidt@gmail.com>
2019-05-14 13:58:36 +0200

    added tables to the database + some minor changes to a template

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -64,6 +64,22 @@ CREATE TABLE tag (
     name text
 );
 
+DROP TABLE IF EXISTS public.user_account;
+DROP SEQUENCE IF EXISTS public.user_id_seq;
+CREATE TABLE user_account (
+    id serial NOT NULL,
+    name text,
+    password_hash text,
+    role_id integer,
+    registration_date timestamp without time zone
+);
+
+DROP TABLE IF EXISTS public.user_role;
+DROP SEQUENCE IF EXISTS public.role_id_seq;
+CREATE TABLE user_role (
+    id serial NOT NULL,
+    role text
+);
 
 ALTER TABLE ONLY answer
     ADD CONSTRAINT pk_answer_id PRIMARY KEY (id);
@@ -80,6 +96,12 @@ ALTER TABLE ONLY question_tag
 ALTER TABLE ONLY tag
     ADD CONSTRAINT pk_tag_id PRIMARY KEY (id);
 
+ALTER TABLE ONLY user_account
+    ADD CONSTRAINT pk_user_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY user_role
+    ADD CONSTRAINT pk_role_id PRIMARY KEY (id);
+
 ALTER TABLE ONLY comment
     ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
 
@@ -95,8 +117,8 @@ ALTER TABLE ONLY comment
 ALTER TABLE ONLY question_tag
     ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id);
 
-INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
-INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
+INSERT INTO question(submission_time, view_number, vote_number, title, message, image) VALUES ('2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
+INSERT INTO question(submission_time, view_number, vote_number, title, message, image) VALUES ('2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
 
 I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
 
@@ -105,23 +127,24 @@ BUT in my theme i also using jquery via webpack so the loading order is now foll
 jquery
 booklet
 app.js (bundled file with webpack, including jquery)', 'images/image1.png');
-INSERT INTO question VALUES (2, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
+INSERT INTO question(submission_time, view_number, vote_number, title, message, image) VALUES ('2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
 ', NULL);
-SELECT pg_catalog.setval('question_id_seq', 2, true);
 
-INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
-INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
-SELECT pg_catalog.setval('answer_id_seq', 2, true);
+INSERT INTO answer(submission_time, vote_number, question_id, message, image) VALUES ('2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
+INSERT INTO answer(submission_time, vote_number, question_id, message, image) VALUES ('2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
 
-INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
-INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
-SELECT pg_catalog.setval('comment_id_seq', 2, true);
+INSERT INTO comment(question_id, message, submission_time) VALUES (1, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
+INSERT INTO comment(answer_id, message, submission_time) VALUES (1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
 
-INSERT INTO tag VALUES (1, 'python');
-INSERT INTO tag VALUES (2, 'sql');
-INSERT INTO tag VALUES (3, 'css');
-SELECT pg_catalog.setval('tag_id_seq', 3, true);
+INSERT INTO tag(name) VALUES ('python');
+INSERT INTO tag(name) VALUES ('sql');
+INSERT INTO tag(name) VALUES ('css');
 
-INSERT INTO question_tag VALUES (0, 1);
-INSERT INTO question_tag VALUES (1, 3);
-INSERT INTO question_tag VALUES (2, 3);
+INSERT INTO question_tag VALUES (1, 1);
+INSERT INTO question_tag VALUES (2, 2);
+INSERT INTO question_tag VALUES (3, 3);
+
+INSERT INTO user_role(role) VALUES ('admin');
+INSERT INTO user_role(role) VALUES ('user');
+
+
