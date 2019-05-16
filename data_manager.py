19c218f7b321d2acd81b710c0c41f01a0402f7ea
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-05-16 12:53:19 +0200

    make apostrophes and sql codes consistent

--- data_manager.py
+++ data_manager.py
@@ -6,24 +6,26 @@ import bcrypt
 
 @connection.connection_handler
 def get_max_id(cursor, table):
-    cursor.execute(
-        f"""SELECT MAX(id) FROM {table};""")
+    cursor.execute("""
+                   SELECT MAX(id) FROM %(table)s;
+                   """,
+                   {'table': table})
     id_ = cursor.fetchone()
     return id_
 
 
 @connection.connection_handler
-def update_vote_number(cursor, table, oper, id_):
-    if oper not in "+1-1":
-        raise ValueError(f"{oper} should be +1 or -1.")
+def update_vote_number(cursor, table, operation, id_):
+    if operation not in '+1-1':
+        raise ValueError(f'{operation} should be +1 or -1.')
     cursor.execute((
-        sql.SQL("UPDATE {} SET vote_number = vote_number " + oper + " WHERE id=%(id_)s").
-        format(sql.Identifier(table))), {"id_": id_})
+        sql.SQL('UPDATE {} SET vote_number = vote_number ' + operation + ' WHERE id=%(id_)s').
+        format(sql.Identifier(table))), {'id_': id_})
 
 
 @connection.connection_handler
 def insert_new_record(cursor, table, record):
-    record['submission_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    record['submission_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     values = ""
     for value in record.values():
         if type(value) is str:
@@ -31,14 +33,14 @@ def insert_new_record(cursor, table, record):
         values += (f"'{value}'" if type(value) is str else f"{value}") + ', '
     values = values[:-2] + ')'
     keys = str(tuple(record.keys())).replace("'","")
-    cursor.execute(sql.SQL("INSERT INTO {table} " + keys + " VALUES (" + values).
+    cursor.execute(sql.SQL('INSERT INTO {table} ' + keys + ' VALUES (' + values).
                    format(table=sql.Identifier(table)))
 
 
 @connection.connection_handler
 def get_record_by_id(cursor, table, _id):
-    cursor.execute(sql.SQL("SELECT * FROM {table} WHERE id=%(_id)s").
-                   format(table=sql.Identifier(table)), {"_id": _id})
+    cursor.execute(sql.SQL('SELECT * FROM {table} WHERE id=%(_id)s').
+                   format(table=sql.Identifier(table)), {'_id': _id})
     record = cursor.fetchone()
     return record
 
@@ -49,10 +51,11 @@ def get_question_with_user_info(cursor, question_id):
                    SELECT question.id, user_account.name AS posted_by,
                    question.submission_time, question.view_number, question.vote_number,
                    question.title, question.message, question.image
-                   FROM question JOIN user_account ON question.user_id = user_account.id
+                   FROM question
+                   JOIN user_account ON question.user_id = user_account.id
                    WHERE question.id = %(question_id)s
                    """,
-                   {"question_id": question_id})
+                   {'question_id': question_id})
     record = cursor.fetchone()
     return record
 
@@ -63,10 +66,11 @@ def get_answer_with_user_info(cursor, answer_id):
                    SELECT answer.id, user_account.name AS posted_by,
                    answer.submission_time, answer.vote_number, answer.question_id,
                    answer.message, answer.image
-                   FROM answer JOIN user_account ON answer.user_id = user_account.id
+                   FROM answer
+                   JOIN user_account ON answer.user_id = user_account.id
                    WHERE answer.id = %(answer_id)s
                    """,
-                   {"answer_id": answer_id})
+                   {'answer_id': answer_id})
     record = cursor.fetchone()
     return record
 
@@ -77,9 +81,11 @@ def get_comment_by_parent_id(cursor, parent_type, parent_id):
                            SELECT comment.id, user_account.name AS posted_by,
                            comment.question_id, comment.answer_id,
                            comment.message, comment.submission_time, comment.edited_count
-                           FROM comment JOIN user_account ON comment.user_id = user_account.id
+                           FROM comment
+                           JOIN user_account ON comment.user_id = user_account.id
                            WHERE {parent_type} = %(parent_id)s
-                           """).format(parent_type=sql.Identifier(parent_type)), {"parent_id": parent_id})
+                           """).format(parent_type=sql.Identifier(parent_type)),
+                   {'parent_id': parent_id})
     records = cursor.fetchall()
     return records
 
@@ -90,10 +96,12 @@ def get_answers_by_question_id(cursor, question_id):
                    SELECT answer.id, user_account.name AS posted_by,
                    answer.submission_time, answer.vote_number, answer.question_id,
                    answer.message, answer.image
-                   FROM answer JOIN user_account ON answer.user_id = user_account.id
-                   WHERE question_id=%(question_id)s ORDER BY vote_number DESC
+                   FROM answer
+                   JOIN user_account ON answer.user_id = user_account.id
+                   WHERE question_id=%(question_id)s
+                   ORDER BY vote_number DESC
                    """,
-                   {"question_id": question_id})
+                   {'question_id': question_id})
     records = cursor.fetchall()
     return records
 
@@ -101,40 +109,51 @@ def get_answers_by_question_id(cursor, question_id):
 @connection.connection_handler
 def update_comment_by_primary_id(cursor, data, id):
     count, msg = data['edited_count'], data['message']
-    cursor.execute(
-        """UPDATE comment SET edited_count= %(count)s, message= %(msg)s
-            WHERE id=%(id)s""", {"count": count, "msg": msg, "id": id})
+    cursor.execute("""
+                   UPDATE comment
+                   SET edited_count= %(count)s, message= %(msg)s
+                   WHERE id=%(id)s
+                   """,
+                   {'count': count, 'msg': msg, 'id': id})
 
 
 @connection.connection_handler
 def update_answer(cursor, message, image_url, id):
-    cursor.execute(
-        """UPDATE answer SET message= %(message)s, image= %(image_url)s
-            WHERE id=%(id)s""", {"message": message, "image_url": image_url, "id": id})
+    cursor.execute("""
+                   UPDATE answer
+                   SET message= %(message)s, image= %(image_url)s
+                   WHERE id=%(id)s
+                   """,
+                   {'message': message, 'image_url': image_url, 'id': id})
 
 
 @connection.connection_handler
 def update_question(cursor, title, message, image, id_):
-    cursor.execute(
-        """ UPDATE question SET title= %(title)s, message= %(message)s, image= %(image)s
-            WHERE id=%(id_)s;""", {"title": title, "message": message, "image": image, "id_": id_})
+    cursor.execute("""
+                   UPDATE question
+                   SET title= %(title)s, message= %(message)s, image= %(image)s
+                   WHERE id=%(id_)s;
+                   """,
+                   {'title': title, 'message': message, 'image': image, 'id_': id_})
 
 
 @connection.connection_handler
 def get_sorted_questions(cursor, column_to_order_by, asc=True, limit=None):
     order_direction = 'ASC' if asc is True else 'DESC'
-    command = f"""SELECT question.id, submission_time, user_account.name
-                  AS posted_by, view_number, vote_number, title, message, image
-                  FROM question FULL JOIN user_account ON user_id = user_account.id
-                  WHERE submission_time IS NOT NULL
-                  ORDER BY {column_to_order_by} {order_direction}
-                  """
+    command = f"""
+               SELECT question.id, submission_time, user_account.name
+               AS posted_by, view_number, vote_number, title, message, image
+               FROM question
+               FULL JOIN user_account ON user_id = user_account.id
+               WHERE submission_time IS NOT NULL
+               ORDER BY {column_to_order_by} {order_direction}
+               """
     if limit:
-        cursor.execute("SELECT COUNT(*) FROM question;")
+        cursor.execute('SELECT COUNT(*) FROM question;')
         row_num = cursor.fetchone()['count']
         if row_num < limit:
             limit = row_num
-        command += f" OFFSET {row_num} - {limit} FETCH FIRST {limit} ROWS ONLY"
+        command += f' OFFSET {row_num} - {limit} FETCH FIRST {limit} ROWS ONLY'
     cursor.execute(command)
     ordered_table = cursor.fetchall()
     return ordered_table
@@ -142,33 +161,45 @@ def get_sorted_questions(cursor, column_to_order_by, asc=True, limit=None):
 
 @connection.connection_handler
 def get_search_results_from_database(cursor, search_phrase):
-    cursor.execute('''
+    cursor.execute("""
                    SELECT DISTINCT ON (title) title, question.message FROM question
                    JOIN answer ON question.id = answer.question_id
                    WHERE title ILIKE %(search_phrase)s OR question.message
                    ILIKE %(search_phrase)s OR answer.message ILIKE %(search_phrase)s;
-                   ''',
+                   """,
                    {'search_phrase': search_phrase})
     search_results = cursor.fetchall()
     return search_results
 
 
 @connection.connection_handler
-def delete_by_id(cursor, table, id_, parent_="id"):
-    cursor.execute((sql.SQL("DELETE FROM {table} WHERE {parent_} = %(id_)s").
-                    format(table=sql.Identifier(table), parent_=sql.Identifier(parent_))), {"id_": id_})
+def delete_by_id(cursor, table, id_, parent_='id'):
+    cursor.execute((sql.SQL("""
+                            DELETE FROM {table}
+                            WHERE {parent_} = %(id_)s
+                            """).
+                    format(table=sql.Identifier(table), parent_=sql.Identifier(parent_))),
+                   {'id_': id_})
 
 
 @connection.connection_handler
 def get_answer_ids(cursor, question_id):
-    cursor.execute("SELECT id FROM answer WHERE question_id=%(question_id)s", {"question_id": question_id})
+    cursor.execute("""
+                   SELECT id FROM answer
+                   WHERE question_id=%(question_id)s
+                   """,
+                   {'question_id': question_id})
     answer_ids = cursor.fetchall()
     return answer_ids
 
 
 @connection.connection_handler
 def get_tag_by_question_id(cursor, id_):
-    cursor.execute("SELECT * FROM question_tag WHERE question_id=%(id_)s", {"id_": id_})
+    cursor.execute("""
+                   SELECT * FROM question_tag
+                   WHERE question_id=%(id_)s
+                   """,
+                   {'id_': id_})
     records = cursor.fetchall()
     return records
 
@@ -176,34 +207,49 @@ def get_tag_by_question_id(cursor, id_):
 @connection.connection_handler
 def insert_new_tag(cursor, new_tag):
     new_tag_name = new_tag['name']
-    cursor.execute(
-        """ INSERT INTO tag (name) 
-        VALUES (%(new_tag_name)s)""", {"new_tag_name": new_tag_name})
+    cursor.execute("""
+                   INSERT INTO tag (name)
+                   VALUES (%(new_tag_name)s)
+                   """,
+                   {'new_tag_name': new_tag_name})
     new_tag_id = get_max_id('tag')['max']
     new_tag_question_id = new_tag['question_id']
-    cursor.execute(
-        """ INSERT INTO question_tag (question_id, tag_id) 
-        VALUES (%(new_tag_question_id)s, %(new_tag_id)s)""", {"new_tag_question_id": new_tag_question_id, "new_tag_id": new_tag_id})
+    cursor.execute("""
+                   INSERT INTO question_tag (question_id, tag_id)
+                   VALUES (%(new_tag_question_id)s, %(new_tag_id)s)
+                   """,
+                   {'new_tag_question_id': new_tag_question_id, 'new_tag_id': new_tag_id})
 
 
 @connection.connection_handler
 def delete_tags(cursor, question_id, tag):
-    cursor.execute("""DELETE FROM question_tag WHERE question_id=%(question_id)s;
-            DELETE FROM tag WHERE id=%(tag)s;""", {"question_id": question_id, "tag": tag})
+    cursor.execute("""
+                   DELETE FROM question_tag
+                   WHERE question_id=%(question_id)s;
+                   DELETE FROM tag
+                   WHERE id=%(tag)s;
+                   """,
+                   {'question_id': question_id, 'tag': tag})
 
 
 @connection.connection_handler
 def update_tag(cursor, tag):
     tag_name = tag['name']
     tag_id = tag['tag_id']
-    cursor.execute(
-        """ UPDATE tag SET name=%(tag_name)s 
-        WHERE id=%(tag_id)s;""", {"tag_name": tag_name, "tag_id": tag_id})
+    cursor.execute("""
+                   UPDATE tag
+                   SET name=%(tag_name)s
+                   WHERE id=%(tag_id)s;
+                   """,
+                   {'tag_name': tag_name, 'tag_id': tag_id})
 
 
 @connection.connection_handler
 def get_basic_tags(cursor):
-    cursor.execute("SELECT * FROM tag WHERE id <= 9")
+    cursor.execute("""
+                   SELECT * FROM tag
+                   WHERE id <= 9
+                   """)
     basic_tags = cursor.fetchall()
     tag_s = []
     for tag in basic_tags:
@@ -237,17 +283,17 @@ def get_password_hash_by_name(cursor, name):
 def get_role_id_if_user_exists(cursor, name):
     cursor.execute("""
                    SELECT role_id FROM user_account
-                   WHERE name LIKE %(name)s;
+                   WHERE name=%(name)s;
                    """,
                    {'name': name})
-    role_in_dict = cursor.fetchone()
-    return role_in_dict
+    role_id = cursor.fetchone()
+    return role_id['role_id'] if role_id else None
 
 
 @connection.connection_handler
 def register_user(cursor, name, password):
-    role_in_dict = get_role_id_if_user_exists(name)
-    if role_in_dict:
+    role_id = get_role_id_if_user_exists(name)
+    if role_id:
         pass
     else:
         hashed_bytes = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
@@ -262,6 +308,10 @@ def register_user(cursor, name, password):
 
 @connection.connection_handler
 def get_user_id_by_user_name(cursor, name):
-    cursor.execute("SELECT id FROM user_account WHERE name=%(name)s", {'name': name})
+    cursor.execute("""
+                   SELECT id FROM user_account
+                   WHERE name=%(name)s
+                   """,
+                   {'name': name})
     user_id = cursor.fetchone()
     return user_id
--- server.py
+++ server.py
@@ -6,90 +6,90 @@ app = Flask(__name__)
 app.secret_key = '\xc1N}\xd3\xf9\x15\xa3\n*7i\xa6'
 
 
-@app.route("/")
-@app.route("/list")
+@app.route('/')
+@app.route('/list')
 def route_questions_list():
-    order_by = request.args.get("order_by")
+    order_by = request.args.get('order_by')
     if not order_by:
-        order_by = "submission_time"
-    direction_ = request.args.get("order_direction")
-    direction_ = True if direction_ == "asc" else False
+        order_by = 'submission_time'
+    direction_ = request.args.get('order_direction')
+    direction_ = True if direction_ == 'asc' else False
     limit = 5 if request.path == '/' else None
     question_list = data_manager.get_sorted_questions(order_by, direction_, limit)
     return render_template('index.html', question_list=question_list, current_dir=direction_)
 
 
-@app.route("/question/<question_id>/vote-<operation>", methods=["GET", "POST"])
+@app.route('/question/<question_id>/vote-<operation>', methods=['GET', 'POST'])
 def route_question_votes(question_id, operation):
-    if request.method == "POST":
-        op_change = "+1" if operation == "up" else "-1"
-        data_manager.update_vote_number("question", op_change, question_id)
+    if request.method == 'POST':
+        op_change = '+1' if operation == 'up' else '-1'
+        data_manager.update_vote_number('question', op_change, question_id)
     return redirect(request.referrer)
 
 
-@app.route("/answer/<answer_id>/vote-<operation>",  methods=["GET", "POST"])
+@app.route('/answer/<answer_id>/vote-<operation>',  methods=['GET', 'POST'])
 def route_answer_votes(answer_id, operation):
-    if request.method == "POST":
-        op_change = "+1" if operation == "up" else "-1"
-        data_manager.update_vote_number("answer", op_change, answer_id)
-        answer = data_manager.get_record_by_id("answer", answer_id)
-        return redirect(url_for("route_question_display", question_id=answer["question_id"]))
-    return redirect("")
+    if request.method == 'POST':
+        op_change = '+1' if operation == 'up' else '-1'
+        data_manager.update_vote_number('answer', op_change, answer_id)
+        answer = data_manager.get_record_by_id('answer', answer_id)
+        return redirect(url_for('route_question_display', question_id=answer['question_id']))
+    return redirect('')
 
 
-@app.route("/question/<question_id>", methods=['GET', 'POST'])
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
 def route_question_display(question_id):
     tag = data_manager.get_tag_by_question_id(question_id)
     if tag == []:
         current_tag = None
     else:
         tag = tag[0]['tag_id']
-        current_tag = data_manager.get_record_by_id("tag", tag)
+        current_tag = data_manager.get_record_by_id('tag', tag)
     if request.method == 'POST':
         new_q = request.form.to_dict()
         data_manager.update_question(new_q['title'], new_q['message'], new_q['image'], question_id)
         return redirect('/question/%s' % question_id)
-    template_name = "record_details.html"
+    template_name = 'record_details.html'
     question = data_manager.get_question_with_user_info(question_id)
     if question is None:
         return render_template(template_name, question_id=question_id)
     answers = data_manager.get_answers_by_question_id(question_id)
-    comments = data_manager.get_comment_by_parent_id("question_id", question_id)
+    comments = data_manager.get_comment_by_parent_id('question_id', question_id)
     return render_template(template_name, question=question, answers=answers, comments=comments, tag=current_tag)
 
 
 @app.route('/answer/<answer_id>')
 def route_answer_display(answer_id):
     answer = data_manager.get_answer_with_user_info(answer_id)
-    comments = data_manager.get_comment_by_parent_id("answer_id", answer_id)
+    comments = data_manager.get_comment_by_parent_id('answer_id', answer_id)
     return render_template('record_details.html', answer=answer, comments=comments)
 
 
-@app.route("/question/<question_id>/edit", methods=['GET', 'POST'])
+@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
 def route_edit_question(question_id):
-    question = data_manager.get_record_by_id("question", question_id)
+    question = data_manager.get_record_by_id('question', question_id)
     if question is None:
         return render_template('record_details.html', question_id=question_id)
     else:
         return render_template('add_question.html', question=question)
 
 
-@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def route_add_answer(question_id):
     if 'name' not in session:
         return render_template('warning.html', message='You need to log in to add an answer')
-    if request.method == "POST":
+    if request.method == 'POST':
         new_answer = request.form.to_dict()
-        new_answer["vote_number"] = 0
-        new_answer["question_id"] = question_id
+        new_answer['vote_number'] = 0
+        new_answer['question_id'] = question_id
         user_id_dict = data_manager.get_user_id_by_user_name(session['name'])
         new_answer['user_id'] = user_id_dict['id']
         data_manager.insert_new_record('answer', new_answer)
-        return redirect(f"/question/{question_id}")
-    return render_template("add_edit.html", parent_id=question_id, parent='question', type='answer')
+        return redirect(f'/question/{question_id}')
+    return render_template('add_edit.html', parent_id=question_id, parent='question', type='answer')
 
 
-@app.route("/question/<question_id>/delete", methods=["GET", "POST"])
+@app.route('/question/<question_id>/delete', methods=['GET', 'POST'])
 def route_delete_question(question_id):
     answer_ids = data_manager.get_answer_ids(question_id)
     for answer_id in answer_ids:
@@ -98,18 +98,18 @@ def route_delete_question(question_id):
     data_manager.delete_by_id('question_tag', question_id, 'question_id')
     data_manager.delete_by_id('comment', question_id, 'question_id')
     data_manager.delete_by_id('question', question_id)
-    return redirect("/")
+    return redirect('/')
 
 
-@app.route("/answer/<answer_id>/delete", methods=["GET", "POST"])
+@app.route('/answer/<answer_id>/delete', methods=['GET', 'POST'])
 def route_delete_answer(answer_id):
-    answer = data_manager.get_record_by_id("answer", answer_id)
-    question_id = answer["question_id"]
+    answer = data_manager.get_record_by_id('answer', answer_id)
+    question_id = answer['question_id']
     data_manager.delete_by_id('answer', answer_id)
-    return redirect(url_for("route_question_display", question_id=question_id))
+    return redirect(url_for('route_question_display', question_id=question_id))
 
 
-@app.route("/add-question", methods=['GET', 'POST'])
+@app.route('/add-question', methods=['GET', 'POST'])
 def route_question_add():
     if 'name' not in session:
         return render_template('warning.html', message='You need to log in to ask a question')
@@ -137,7 +137,7 @@ def route_add_comment_to_question(question_id):
     return render_template('add_edit.html', parent_id=question_id, parent='question', type='comment')
 
 
-@app.route("/answer/<answer_id>/new_comment", methods=['GET', 'POST'])
+@app.route('/answer/<answer_id>/new_comment', methods=['GET', 'POST'])
 def route_add_comment_to_answer(answer_id):
     if 'name' not in session:
         return render_template('warning.html', message='You need to log in to add a comment')
@@ -145,8 +145,8 @@ def route_add_comment_to_answer(answer_id):
     if new_comment:
         user_id_dict = data_manager.get_user_id_by_user_name(session['name'])
         new_comment['user_id'] = user_id_dict['id']
-        data_manager.insert_new_record("comment", new_comment)
-        return redirect(url_for("route_answer_display", answer_id=answer_id))
+        data_manager.insert_new_record('comment', new_comment)
+        return redirect(url_for('route_answer_display', answer_id=answer_id))
     return render_template('add_edit.html', parent_id=answer_id, parent='answer', type='comment')
 
 
@@ -158,7 +158,7 @@ def route_edit_answer(answer_id):
         image = request.form.get('image')
         data_manager.update_answer(message, image, answer_id)
         return redirect(url_for('route_question_display', question_id=question_id))
-    answer = data_manager.get_record_by_id("answer", answer_id)
+    answer = data_manager.get_record_by_id('answer', answer_id)
     return render_template('add_edit.html', data=answer, id=answer_id, type='answer')
 
 
@@ -175,11 +175,11 @@ def route_edit_comment(comment_id):
             return redirect(url_for('route_question_display', question_id=parent_id))
         parent_id = request.form.get('answer_id')
         return redirect(url_for('route_answer_display', answer_id=parent_id))
-    comment = data_manager.get_record_by_id("comment", comment_id)
+    comment = data_manager.get_record_by_id('comment', comment_id)
     return render_template('add_edit.html', data=comment, id=comment_id, type='comment')
 
 
-@app.route("/search")
+@app.route('/search')
 def route_search_results():
     search_phrase = request.args.get('search_phrase')
     if search_phrase == '':
@@ -192,7 +192,7 @@ def route_search_results():
 @app.route('/comments/<comment_id>/delete', methods=['POST', 'GET'])
 def route_delete_comment(comment_id):
     if request.method == 'POST':
-        comment = data_manager.get_record_by_id("comment", comment_id)
+        comment = data_manager.get_record_by_id('comment', comment_id)
         data_manager.delete_by_id('comment', comment_id)
         if comment['question_id'] is not None:
             return redirect(f"/question/{comment['question_id']}")
@@ -200,7 +200,7 @@ def route_delete_comment(comment_id):
     return render_template('warning.html', message='Action denied')
 
 
-@app.route("/question/<question_id>/add-edit-tag", methods=['GET', 'POST'])
+@app.route('/question/<question_id>/add-edit-tag', methods=['GET', 'POST'])
 def route_add_edit_tag(question_id):
     tags = data_manager.get_basic_tags()
     tag_id = data_manager.get_tag_by_question_id(question_id)
@@ -208,45 +208,44 @@ def route_add_edit_tag(question_id):
         tag_id = None
     else:
         tag_id = tag_id[0]['tag_id']
-        tag_id = data_manager.get_record_by_id("tag", tag_id)
-    if request.method == "POST":
+        tag_id = data_manager.get_record_by_id('tag', tag_id)
+    if request.method == 'POST':
         ntag = request.form.to_dict()
         question_id = ntag['question_id']
-        if ntag['define_own'] == "":
+        if ntag['define_own'] == '':
             del ntag['define_own']
             ntag['name'] = ntag.pop('select')
         else:
             del ntag['select']
             ntag['name'] = ntag.pop('define_own')
-        if ntag['tag_id'] == "":
+        if ntag['tag_id'] == '':
             data_manager.insert_new_tag(ntag)
         else:
             data_manager.update_tag(ntag)
-        return redirect(f"/question/{question_id}")
+        return redirect(f'/question/{question_id}')
     return render_template('add_edit_tag.html', question_id=question_id, tags=tags, tag_id=tag_id)
 
 
-@app.route("/question/<question_id>/<tag>/delete")
+@app.route('/question/<question_id>/<tag>/delete')
 def route_delete_tag(question_id, tag):
     data_manager.delete_tags(question_id, tag)
-    return redirect(f"/question/{question_id}")
+    return redirect(f'/question/{question_id}')
 
 
-@app.route("/registration", methods=['GET', 'POST'])
+@app.route('/registration', methods=['GET', 'POST'])
 def route_register_user():
     if request.method == 'POST':
         user_data = request.form.to_dict()
         data_manager.register_user(user_data['name'], user_data['password'])
-        return redirect(url_for("route_questions_list"))
+        return redirect(url_for('route_questions_list'))
     return render_template('registration.html', type='registration')
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def route_login():
     if request.method == 'POST':
-        role_in_dict = data_manager.get_role_id_if_user_exists(request.form['name'])
-        if role_in_dict:
-            role_id = role_in_dict['role_id']
+        role_id = data_manager.get_role_id_if_user_exists(request.form['name'])
+        if role_id:
             password_hash = data_manager.get_password_hash_by_name(request.form['name'])
             valid_user_data = data_manager.verify_password(request.form['password'], password_hash)
             if valid_user_data:
@@ -256,12 +255,12 @@ def route_login():
     return render_template('registration.html', type='login')
 
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     session.pop('name', None)
     session.pop('role_id', None)
     return redirect(url_for('route_questions_list'))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
